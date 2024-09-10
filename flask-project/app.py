9d4f9de16fd76f03b1ba50f7d3d4a688314c96da
Pedro Alves <pedrohvs.alves@gmail.com>
2024-09-10 12:03:10 -0300

    Salvando alterações antes do rebase

--- flask-project/app.py
+++ flask-project/app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+@app.route('/visao-geral')
+def geral():
+    return render_template('visao-geral.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
