e4c15b4d881dde1eee580010b1cb5cc171d61000
pedro-h-martins <pedrohenrimartinss@gmail.com>
2024-09-17 21:01:39 -0300

    Mudei <div> pra <article>, coloquei para manter o botão precionado, mudei a maior parte dos valores px para rem, alterei o  css do input date

--- flask-project/static/js/filtro.js
+++ flask-project/static/js/filtro.js
@@ -1,15 +1,10 @@
 let buttons = document.querySelectorAll(".btn-tipo");
 
-document.addEventListener("click", function(evt){
+document.addEventListener("click", function(evt) {
 
-  if(evt.target.classList.contains("btn-tipo")){
+  if (evt.target.classList.contains("btn-tipo")) {
 
-
-    buttons.forEach(function(button){
-      button.classList.remove("ativo");
-    });
-
-    evt.target.classList.add("ativo");
+    evt.target.classList.toggle("ativo");
     
   }
 });
