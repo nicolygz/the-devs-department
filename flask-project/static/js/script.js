355b7af8a793d3a6d45e0fe6cf3b9fdf26fa0b70
Pedro Martins <pedrohenrimartinss@gmail.com>
2024-10-22 09:27:49 -0300

    Coloquei os  scripts no script.js e arrumei o limite do email, diminuindo a fonte

--- flask-project/static/js/script.js
+++ flask-project/static/js/script.js
@@ -7,14 +7,14 @@ function showContent(contentType) {
     proposicoes.classList.remove('show');
 
     // Aguarda a animação de "fade out" antes de esconder completamente
-    setTimeout(function() {
+    setTimeout(function () {
         resumo.style.display = 'none';
         proposicoes.style.display = 'none';
-        
+
         // Mostra o conteúdo com animação "fade in"
         var selectedContent = document.getElementById(contentType);
         selectedContent.style.display = 'block';
-        setTimeout(function() {
+        setTimeout(function () {
             selectedContent.classList.add('show');
         }, 10); // Leve delay para garantir o efeito
     }, 500); // Tempo igual ao `transition` do CSS (0.5s)
@@ -25,4 +25,46 @@ document.getElementById('resumo').classList.add('show');
 document.getElementById('resumo').style.display = 'block';
 
 const stars = document.querySelectorAll('.stars input');
+
+// Atualização do Botão
+let isUpdating = false; // Variável para controlar o estado de atualização
+
+function updateData(event) {
+    event.preventDefault(); // Previne o comportamento padrão do link
+
+    if (isUpdating) return; // Se já estiver atualizando, não faz nada
+
+    isUpdating = true; // Indica que a atualização está em andamento
+    const btnContainer = document.getElementById("btnContainer");
+
+    // Muda a cor de fundo do contêiner do botão
+    btnContainer.style.backgroundColor = "#ffa100";
+
+    // Faz a requisição para o servidor
+    fetch(document.getElementById("updateButton").href)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error('Erro na requisição');
+            }
+        })
+        .then(data => {
+            console.log(data);
+        })
+        .catch(error => {
+            console.error('Erro:', error);
+        })
+        .finally(() => {
+            // Restaura a cor original após a requisição
+            btnContainer.style.backgroundColor = "";
+
+            // Espera 30 segundos antes de permitir outra atualização
+            setTimeout(() => {
+                isUpdating = false; // Permite nova atualização
+            }, 30000);
+        });
+}
+
+
 
